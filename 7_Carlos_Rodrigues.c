fe8732f5676f19a4dd6184fa79b6d9a21ab33214
Carlos Rodrigues Católica <carlos.rodrigues@catolicasc.edu.br>
2025-03-21 13:22:29 -0300

    7_Carlos_Rodrigues.c

--- 7_Carlos_Rodrigues.c
+++ 7_Carlos_Rodrigues.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
-  int valor, antecessor, sucessor;
-  
-    printf("Me informe um valor inteiro: ");
-    scanf("%d", &valor);
-    
-     antecessor = valor - 1;
-     sucessor = valor + 1;
-  
-    printf("O antecessor de %d eh: %d\n", valor, antecessor);
-    printf("O sucessor de %d eh: %d\n", valor, sucessor);
 
+  
+    float dolar, reais, conversao;
+    printf("Me informe o valor do câmbio do dólar do dia de hoje: ");
+
+    scanf("%f", &dolar);
+    printf("Me informe o valor de dólares que você quer: ");
+    scanf("%f", &reais);
+    conversao = dolar * reais;
+    printf("O valor em reais é: %.2f", conversao);
+    
     return 0;
 }
